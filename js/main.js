25d43d766cd0118d1316969132312a896db32525
kosh-ilya <kosh-ilya@mail.ru>
2021-03-08 17:01:20 +0300

    create: add slider in hotel

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  effect: "coverflow",
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button__next",
+    prevEl: ".slider-button__prev",
+  },
+});
