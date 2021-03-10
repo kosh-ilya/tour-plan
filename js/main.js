4008990d93968e22d75f35cb8240b7c42504c864
kosh-ilya <kosh-ilya@mail.ru>
2021-03-10 14:28:36 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper-container", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
   loopedSlides: 1,
@@ -7,8 +7,23 @@ const swiper = new Swiper(".swiper-container", {
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button_next",
-    prevEl: ".slider-button_prev",
+    nextEl: ".hotel-slider__button_next",
+    prevEl: ".hotel-slider__button_prev",
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+  loopedSlides: 1,
+  slidesPerView: 1,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   keyboard: {
     enabled: true,
